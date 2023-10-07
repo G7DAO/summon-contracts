5a5a8c55124fe8f5900e5c96d114416ba2e7746c
Omar Garcia <omargarcia@Omars-MacBook-Pro-2.local>
2023-10-07 15:01:31 -0300

    feat: update deploy script

--- scripts/deployGameSummary1155.ts
+++ scripts/deployGameSummary1155.ts
@@ -1,7 +1,7 @@
 import { log } from '@helpers/logger';
 import { ethers } from 'hardhat';
 
-import { GameAchievements } from '../typechain-types';
+import { GameAchievements, GameSummary1155 } from '../typechain-types';
 import { verifyContract } from '@helpers/verify';
 
 async function main() {
@@ -11,7 +11,7 @@ async function main() {
     throw new Error('PRIVATE_KEY env required');
   }
 
-  if(!PUBLIC_KEY) {
+  if (!PUBLIC_KEY) {
     throw new Error('PUBLIC_KEY env required');
   }
 
@@ -19,26 +19,33 @@ async function main() {
   const signer = new ethers.Wallet(PRIVATE_KEY, provider);
   const gameAchievementsContract = await ethers.getContractFactory('GameSummary1155', signer);
 
-  const gameAchievements = (await gameAchievementsContract.deploy('https://achievo.mypinata.cloud/ipfs/')) as unknown as GameAchievements;
+  const gameAchievements = (await gameAchievementsContract.deploy('https://achievo.mypinata.cloud/ipfs/')) as unknown as GameSummary1155;
   await gameAchievements.deployed();
   log('GameSummary1155 deployed:', gameAchievements.address);
-  log('Verifing contract ... ');
-  await verifyContract({
-    contractAddress: gameAchievements.address,
-    constructorArguments: ['https://achievo.mypinata.cloud/ipfs/'],
-    signer,
-    txHash: gameAchievements.deployTransaction.hash,
-  });
 
-  const hasRole = await gameAchievements.hasRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', PUBLIC_KEY);
-
-  log(`Has role: ${hasRole}`);
-
-  if (!hasRole) {
-    const tx = await gameAchievements.grantRole('0x9f2df0fed2c77648de5860a4cc508cd0818c85b8b8a1ab4ceeef8d981c8956a6', PUBLIC_KEY);
+  const minterRole = await gameAchievements.MINTER_ROLE();
+  const hasMinterRole = await gameAchievements.hasRole(minterRole, PUBLIC_KEY);
+  if (!hasMinterRole) {
+    const tx = await gameAchievements.grantRole(minterRole, PUBLIC_KEY);
     await tx.wait();
-    log('Role granted');
+    log('Minter role granted');
   }
+
+  const gameCreatorRole = await gameAchievements.GAME_CREATOR_ROLE();
+  const hasGameCreatorRole = await gameAchievements.hasRole(gameCreatorRole, PUBLIC_KEY);
+  if (!hasGameCreatorRole) {
+    const tx = await gameAchievements.grantRole(gameCreatorRole, PUBLIC_KEY);
+    await tx.wait();
+    log('Game creator role granted');
+  }
+
+  // log('Verifying contract ... ');
+  // await verifyContract({
+  //   contractAddress: gameAchievements.address,
+  //   constructorArguments: ['https://achievo.mypinata.cloud/ipfs/'],
+  //   signer,
+  //   txHash: gameAchievements.deployTransaction.hash,
+  // });
 }
 
 main().catch((error) => {
