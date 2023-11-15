57bfab3d523486e331c72057876ac53088d4f315
Vasin Limsukhawat <vasin.limsukhawat@gmail.com>
2023-11-15 14:09:42 -0700

    feat: Add souldbound1155 generic deployment script so we can reuse this on the contract that use the same contract

---
+++ deploy/deploy.soulbound1155.generic.ts
@@ -0,0 +1,105 @@
+import fs from 'fs';
+import path from 'path';
+
+import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
+import { HardhatRuntimeEnvironment } from 'hardhat/types';
+
+// TODO: change here if you want to deploy/use to another contract type
+import { SoulBound1155 } from '../typechain-types';
+import { log } from '@helpers/logger';
+import getWallet from './getWallet';
+import { encryptPrivateKey } from '@helpers/encrypt';
+import { submitContractToDB } from '@helpers/submit-contract-to-db';
+import { ChainId, NetworkName, Currency, NetworkExplorer, rpcUrls } from '@constants/network';
+import { DeploymentMap } from 'types/deployment-type';
+import { ConstructorArgs } from '@constants/constructor-args';
+
+// load wallet private key from env file
+const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
+const encryptedPrivateKey = encryptPrivateKey(PRIVATE_KEY);
+
+if (!PRIVATE_KEY) throw '⛔️ Private key not detected! Add it to the .env file!';
+
+export default async function (
+    hre: HardhatRuntimeEnvironment,
+    deploymentArgs: { CONTRACT_NAME: string; CONTRACT_TYPE: string; ABI_PATH: string },
+    constructorArgs: ConstructorArgs
+) {
+    const { CONTRACT_NAME, CONTRACT_TYPE, ABI_PATH } = deploymentArgs;
+    const { name, symbol, baseURI, maxPerMint, isPaused, devWallet, royalty, tenants } = constructorArgs;
+
+    log(`Running deploy script for the ${CONTRACT_NAME} featuring zkSync`);
+
+    const networkName = hre.network.name as NetworkName;
+    const networkNameKey = Object.keys(NetworkName)[Object.values(NetworkName).indexOf(networkName)]; // get NetworkName Key from Value
+    const chainId = ChainId[networkNameKey as keyof typeof ChainId];
+    const rpcUrl = rpcUrls[chainId as keyof typeof rpcUrls];
+    const currency = Currency[networkNameKey as keyof typeof Currency];
+    const blockExplorerBaseUrl = NetworkExplorer[networkNameKey as keyof typeof NetworkExplorer];
+
+    const wallet = await getWallet(PRIVATE_KEY);
+    const deployments: DeploymentMap = {};
+
+    // Create deployer object and load the artifact of the contract you want to deploy.
+    const deployer = new Deployer(hre, wallet);
+    const artifact = await deployer.loadArtifact(CONTRACT_NAME);
+
+    const abiPath = path.resolve(ABI_PATH);
+
+    // Read the file content
+    const abiContent = fs.readFileSync(abiPath, 'utf8');
+    const { abi: contractAbi } = JSON.parse(abiContent);
+
+    for (const tenant of tenants) {
+        const achievoContract = (await deployer.deploy(artifact, [
+            `${tenant}${name}`,
+            symbol,
+            baseURI,
+            maxPerMint,
+            isPaused,
+            devWallet,
+            royalty,
+        ])) as SoulBound1155;
+
+        // Show the contract info.
+        const contractAddress = achievoContract.address;
+        log(`${CONTRACT_TYPE}(${artifact.contractName}) for ${tenant} was deployed to https://explorer.zksync.io/address/${contractAddress}#contract`);
+
+        const verificationId = await hre.run('verify:verify', {
+            address: contractAddress,
+            contract: `contracts/${CONTRACT_NAME}.sol:${CONTRACT_NAME}`,
+            constructorArguments: [`${tenant}${name}`, symbol, baseURI, maxPerMint, isPaused, devWallet, royalty],
+        });
+        log(`Verification ID: ${verificationId}`);
+
+        deployments[tenant] = {
+            dbPayload: {
+                contractAbi,
+                contractAddress,
+                type: CONTRACT_TYPE,
+                active: true,
+                networkName,
+                chainId,
+                rpcUrl,
+                currency,
+                blockExplorerBaseUrl,
+                privateKey: encryptedPrivateKey,
+                publicKey: wallet.address,
+                paymasterAddresses: [],
+                fakeContractAddress: '',
+            },
+            explorerUrl: `${blockExplorerBaseUrl}/address/${contractAddress}#contract`,
+        };
+    }
+
+    // await submitContractToDB(deployments);
+
+    // Define the path to the file
+    const filePath = path.resolve(`deployments-${CONTRACT_TYPE}.json`);
+
+    // Convert deployments to JSON
+    const deploymentsJson = JSON.stringify(deployments, null, 2);
+
+    // Write to the file
+    fs.writeFileSync(filePath, deploymentsJson);
+}
--- deploy/deploySoulBoundBadges.testnet.ts
+++ deploy/deploySoulBoundBadges.testnet.ts
@@ -1,104 +1,20 @@
-import fs from 'fs';
-import path from 'path';
-
-import { Deployer } from '@matterlabs/hardhat-zksync-deploy';
 import { HardhatRuntimeEnvironment } from 'hardhat/types';
 
-// TODO: change here if you want to deploy/use to another contract type
-import { SoulBound1155 } from '../typechain-types';
-import { log } from '@helpers/logger';
-import getWallet from './getWallet';
 import { SoulBoundBadgesArgs } from '@constants/constructor-args';
-import { encryptPrivateKey } from '@helpers/encrypt';
-import { submitContractToDB } from '@helpers/submit-contract-to-db';
-import { ChainId, NetworkName, Currency, NetworkExplorer, rpcUrls } from '@constants/network';
-import { DeploymentMap } from 'types/deployment-type';
-
-const { name, symbol, baseURI, maxPerMint, isPaused, devWallet, royalty, tenants } = SoulBoundBadgesArgs.TESTNET;
-
-// load wallet private key from env file
-const PRIVATE_KEY = process.env.PRIVATE_KEY || '';
-const encryptedPrivateKey = encryptPrivateKey(PRIVATE_KEY);
+import deploySoulBound1155Generic from './deploy.soulbound1155.generic';
 
 const CONTRACT_NAME = 'SoulBound1155';
 const CONTRACT_TYPE = 'Badges';
 const ABI_PATH = 'artifacts/contracts/SoulBound1155.sol/SoulBound1155.json';
 
-if (!PRIVATE_KEY) throw '⛔️ Private key not detected! Add it to the .env file!';
-
 export default async function (hre: HardhatRuntimeEnvironment) {
-    log(`Running deploy script for the ${CONTRACT_NAME} featuring zkSync`);
-
-    const networkName = hre.network.name as NetworkName;
-    const networkNameKey = Object.keys(NetworkName)[Object.values(NetworkName).indexOf(networkName)]; // get NetworkName Key from Value
-    const chainId = ChainId[networkNameKey as keyof typeof ChainId];
-    const rpcUrl = rpcUrls[chainId as keyof typeof rpcUrls];
-    const currency = Currency[networkNameKey as keyof typeof Currency];
-    const blockExplorerBaseUrl = NetworkExplorer[networkNameKey as keyof typeof NetworkExplorer];
-
-    const wallet = await getWallet(PRIVATE_KEY);
-    const deployments: DeploymentMap = {};
-
-    // Create deployer object and load the artifact of the contract you want to deploy.
-    const deployer = new Deployer(hre, wallet);
-    const artifact = await deployer.loadArtifact(CONTRACT_NAME);
-
-    const abiPath = path.resolve(ABI_PATH);
-
-    // Read the file content
-    const abiContent = fs.readFileSync(abiPath, 'utf8');
-    const { abi: contractAbi } = JSON.parse(abiContent);
-
-    for (const tenant of tenants) {
-        const achievoContract = (await deployer.deploy(artifact, [
-            `${tenant}${name}`,
-            symbol,
-            baseURI,
-            maxPerMint,
-            isPaused,
-            devWallet,
-            royalty,
-        ])) as SoulBound1155;
-
-        // Show the contract info.
-        const contractAddress = achievoContract.address;
-        log(`${CONTRACT_TYPE}(${artifact.contractName}) for ${tenant} was deployed to https://explorer.zksync.io/address/${contractAddress}#contract`);
-
-        const verificationId = await hre.run('verify:verify', {
-            address: contractAddress,
-            contract: `contracts/${CONTRACT_NAME}.sol:${CONTRACT_NAME}`,
-            constructorArguments: [`${tenant}${name}`, symbol, baseURI, maxPerMint, isPaused, devWallet, royalty],
-        });
-        log(`Verification ID: ${verificationId}`);
-
-        deployments[tenant] = {
-            dbPayload: {
-                contractAbi,
-                contractAddress,
-                type: CONTRACT_TYPE,
-                active: true,
-                networkName,
-                chainId,
-                rpcUrl,
-                currency,
-                blockExplorerBaseUrl,
-                privateKey: encryptedPrivateKey,
-                publicKey: wallet.address,
-                paymasterAddresses: [],
-                fakeContractAddress: '',
-            },
-            explorerUrl: `${blockExplorerBaseUrl}/address/${contractAddress}#contract`,
-        };
-    }
-
-    await submitContractToDB(deployments);
-
-    // Define the path to the file
-    const filePath = path.resolve(`deployments-${CONTRACT_TYPE}.json`);
-
-    // Convert deployments to JSON
-    const deploymentsJson = JSON.stringify(deployments, null, 2);
-
-    // Write to the file
-    fs.writeFileSync(filePath, deploymentsJson);
+    await deploySoulBound1155Generic(
+        hre,
+        {
+            CONTRACT_NAME,
+            CONTRACT_TYPE,
+            ABI_PATH,
+        },
+        SoulBoundBadgesArgs.TESTNET
+    );
 }
