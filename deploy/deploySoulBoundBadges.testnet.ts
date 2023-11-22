a30986d53a64e0d02fec3c67109cd4d1a66280e4
ogarciarevett <ogarciarevett@gmail.com>
2023-11-22 03:19:32 -0300

    fix: wth I wrote there

--- deploy/deploySoulBoundBadges.testnet.ts
+++ deploy/deploySoulBoundBadges.testnet.ts
@@ -5,7 +5,7 @@ import deploySoulbound1155Generic from './deploy.soulbound1155.generic';
 
 const CONTRACT_NAME = 'Soulbound1155';
 const CONTRACT_TYPE = 'Badges';
-const ABI_PATH = 'artifacts/contracts/Soulbound1155.sol.sol.sol/Soulbound1155.sol.json';
+const ABI_PATH = 'artifacts/contracts/Soulbound1155.sol/Soulbound1155.sol.json';
 
 export default async function (hre: HardhatRuntimeEnvironment) {
     await deploySoulbound1155Generic(
