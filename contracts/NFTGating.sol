70f0d92a8d58e9dd3e1b2217da16caf1de06633d
ogarciarevett <ogarciarevett@gmail.com>
2023-11-27 02:42:39 -0300

    feat: Adding admin nft gating contract + fixing deploy erc1155 contracts

---
+++ contracts/NFTGating.sol
@@ -0,0 +1,93 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+/**
+ * Author: Omar <omar@game7.io>(https://github.com/ogarciarevett)
+ * Co-Authors: Max <max@game7.io>(https://github.com/vasinl124)
+ */
+
+/*                        .;c;.
+ *                      'lkXWWWXk:.
+ *                    .dXMMMMMMMMWXkc'.
+ *               .,..  ,dKNMMMMMMMMMMN0o,.
+ *             ,dKNXOo'. .;dKNMMMMMMMMMWN0c.
+ *            .kMMMMMWN0o;. .,lkNMMMMMMWKd,
+ *            .OMMMMMMMMMN0x:. .'ckXN0o;. ..
+ *             :ONMMMMMMMMMMWKxc. .... .:d0d.
+ *              .'cxKWMMMMMMMMMWXkl,.  'o0Nk.
+ *            .:l,  .:dKWMMMMMMMMMMNOl,. .;,
+ *            .OMKl.   .;oOXWMMMMMMMMMN0o;.
+ *            .co;.  .;,. .'lOXWMMMMMMMMMWKl.
+ *               .:dOXWWKd;.  'ckXWMMMMMMMMk.
+ *             .c0WMMMMMMMWKd:.  .:xXWMMMWNo.
+ *             ,oONWMMMMMMMMWXOl.  .;okxl'
+ *                .,lkXWMMMMMMMMWXO:
+ *                    .ckKWMMMMMWKd;
+ *                       .:d0X0d:.
+ *                          ...
+ */
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import { ERCSoulbound } from "./ERCSoulbound.sol";
+import { ISoulbound1155 } from "./interfaces/ISoulbound1155.sol";
+import "./ERCSoulbound.sol";
+
+
+contract NFTGating is ERC721URIStorage, ERC721Enumerable, AccessControl, ERCSoulbound, Pausable, ReentrancyGuard {
+    uint256 private _tokenIdCounter;
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    string private baseURI;
+    string private adminTokenURI;
+    string private superAdminTokenURI;
+
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        address devAdmin,
+        string memory _baseUri,
+        string memory _adminTokenURI,
+        string memory _superAdminTokenURI
+    ) ERC721(_name, _symbol) {
+        _setupRole(DEFAULT_ADMIN_ROLE, devAdmin);
+        _setupRole(MINTER_ROLE, devAdmin);
+        baseURI = _baseUri;
+    }
+
+    function mint(address to, bool superAdmin) public onlyRole(MINTER_ROLE) {
+        uint256 tokenId = _tokenIdCounter++;
+        _safeMint(to, tokenId);
+        if(superAdmin) {
+            _setTokenURI(tokenId, superAdminTokenURI);
+        } else {
+            _setTokenURI(tokenId, adminTokenURI);
+        }
+        _soulboundAddress(to);
+    }
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId,
+        uint256 batch
+    ) internal override(ERC721, ERC721Enumerable) soulboundAddressCheck(from) {
+        super._beforeTokenTransfer(from, to, tokenId, batch);
+    }
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {}
+
+    function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721URIStorage, ERC721Enumerable, AccessControl) returns (bool) {
+        return super.supportsInterface(interfaceId);
+    }
+}
