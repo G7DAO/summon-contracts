c75ce257c39b060d866a36865a0bb9f7a85171b4
ogarciarevett <ogarciarevett@gmail.com>
2023-12-01 14:56:05 -0300

    chore: adding initial audit report from nethermind

--- deploy/mints/mintLvlBound.ts
+++ deploy/mints/mintLvlBound.ts
@@ -8,23 +8,23 @@ import { LevelsBoundV1 } from '../../typechain-types';
 const memoryAddresses = [{ walletAddress: '0x7136fbddd4dffa2369a9283b6e90a040318011ca', rank: 1.5 }];
 
 async function main() {
-  const { PRIVATE_KEY } = process.env;
+    const { PRIVATE_KEY } = process.env;
 
-  if (!PRIVATE_KEY) {
-    throw new Error('PRIVATE_KEY env required');
-  }
-  const provider = ethers.provider;
-  const signer = new ethers.Wallet(PRIVATE_KEY, provider);
-  const lvlBound = new ethers.Contract('', abi, signer) as LevelsBoundV1;
+    if (!PRIVATE_KEY) {
+        throw new Error('PRIVATE_KEY env required');
+    }
+    const provider = ethers.provider;
+    const signer = new ethers.Wallet(PRIVATE_KEY, provider);
+    const lvlBound = new ethers.Contract('FILL_ME', abi, signer) as LevelsBoundV1;
 
-  for await (const user of memoryAddresses) {
-    const tx = await lvlBound.levelUp(user.walletAddress, Math.floor(user.rank));
-    await tx.wait();
-    console.log('TX: ', tx.hash);
-  }
+    for await (const user of memoryAddresses) {
+        const tx = await lvlBound.levelUp(user.walletAddress, Math.floor(user.rank));
+        await tx.wait();
+        console.log('TX: ', tx.hash);
+    }
 }
 
 main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
+    console.error(error);
+    process.exitCode = 1;
 });
