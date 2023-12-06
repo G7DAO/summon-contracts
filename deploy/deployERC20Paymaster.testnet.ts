3f7a2a3ce900dc6ce3b45e862fe4c68c7d375415
ogarciarevett <ogarciarevett@gmail.com>
2023-12-06 02:19:45 -0300

    hotfix: fixing paymaster deploy - old script, sorry max :D

--- deploy/deployERC20Paymaster.testnet.ts
+++ deploy/deployERC20Paymaster.testnet.ts
@@ -18,8 +18,6 @@ if (!ERC20_TOKEN_ADDRESS) {
     throw new Error('ERC20 Token address not detected! Add it to the .env file!');
 }
 
-const USER_WALLET_ADDRESS = 'FILLMEEEEEEEE';
-
 const USDC_PRICE_ID = '0x41f3625971ca2ed2263e78573fe5ce23e13d2558ed3f2e47ab0f84fb9e7ae722';
 
 const ETH_PRICE_ID = '0xca80ba6dc32e08d06f1aa886011eed1d77c77be9eb761cc10d72b7d0a2fd57a6';
@@ -33,22 +31,6 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
     const deployer = new Deployer(hre, wallet);
 
-    const erc20Artifact = await deployer.loadArtifact('MockUSDC');
-    const erc20 = await deployer.deploy(erc20Artifact, ['oUSDC', 'oUSDC', 18]);
-
-    // Supplying the ERC20 tokens to the user wallet:
-    const mint5kTx = await erc20.mint(USER_WALLET_ADDRESS, 5000n);
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    await mint5kTx.wait();
-
-    log('Minted 5000 oUSDC for the user wallet');
-
-    await hre.run('verify:verify', {
-        address: erc20.address,
-        contract: `contracts/mocks/MockUSDC.sol:MockUSDC`,
-        constructorArguments: ['oUSDC', 'oUSDC', 18],
-    });
-
     const paymasterArtifact = await deployer.loadArtifact(CONTRACT_NAME);
     const paymaster = (await deployer.deploy(paymasterArtifact, [
         ERC20_TOKEN_ADDRESS,
@@ -57,20 +39,28 @@ export default async function (hre: HardhatRuntimeEnvironment) {
         PYTH_ORACLE_ADDRESS,
     ])) as unknown as Contract;
 
+    await paymaster.waitForDeployment();
+
+    const paymasterAddress = await paymaster.getAddress();
+
+    log(`Paymaster address: ${paymasterAddress}`);
+
     await (
         await deployer.zkWallet.sendTransaction({
             // @ts-ignore-next-line
-            to: paymaster.address,
+            to: paymasterAddress,
             value: ethers.parseEther('0.05'),
         })
     ).wait();
 
+    log(`Paymaster funded with 0.05 ETH`);
+
     await hre.run('verify:verify', {
-        address: paymaster.address,
-        contract: `contracts/${CONTRACT_NAME}.sol:${CONTRACT_NAME}`,
+        address: paymasterAddress,
+        contract: `contracts/paymasters/${CONTRACT_NAME}.sol:${CONTRACT_NAME}`,
         constructorArguments: [ERC20_TOKEN_ADDRESS, USDC_PRICE_ID, ETH_PRICE_ID, PYTH_ORACLE_ADDRESS],
     });
 
     // @ts-ignore-next-line
-    logExplorerAddress(hre.network.config.chainId as number, paymaster.address);
+    logExplorerAddress(hre.network.config.chainId as number, paymasterAddress);
 }
