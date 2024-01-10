508dce5bd8f161bf2a73486d17ad44e6edc88bfd
Brett Cleary <27568879+BrettCleary@users.noreply.github.com>
2024-01-09 22:51:02 -0800

    fix test

--- test/questRewards.test.ts
+++ test/questRewards.test.ts
@@ -10,41 +10,62 @@ describe('QuestReward', function () {
     let gameDeveloperAccount: SignerWithAddress;
     let playerAccount: SignerWithAddress;
     let questReward: QuestRewards;
+    let questRewardContractAddress: string;
     let nonFunToken: NonFunToken;
+    let nonFunTokenContractAddress: string;
 
     beforeEach(async function () {
         const [admin, player, gameDev] = await ethers.getSigners();
         adminAccount = admin;
         playerAccount = player;
         gameDeveloperAccount = gameDev;
+        console.log(
+            'Admin account address: ',
+            adminAccount.address,
+            '\nPlayer account address: ',
+            playerAccount.address,
+            '\nGame dev account address: ',
+            gameDeveloperAccount.address
+        );
 
         const QuestRewardsFactory = await ethers.getContractFactory('QuestRewards');
-        questReward = (await QuestRewardsFactory.deploy(adminAccount)) as QuestRewards;
+        questReward = (await QuestRewardsFactory.deploy(adminAccount.address)) as QuestRewards;
         await questReward.waitForDeployment();
 
         const NonFunTokenFactory = await ethers.getContractFactory('NonFunToken');
-        nonFunToken = (await NonFunTokenFactory.deploy(adminAccount)) as NonFunToken;
+        nonFunToken = (await NonFunTokenFactory.deploy()) as NonFunToken;
         await nonFunToken.waitForDeployment();
 
         await nonFunToken.mintCollectionNFT(gameDeveloperAccount.address, 0);
+        nonFunTokenContractAddress = await nonFunToken.getAddress();
+        questRewardContractAddress = await questReward.getAddress();
     });
 
     it('Deposits and withdraws an ERC721', async function () {
-        // game developer gets signature from admin account to deposit
-        const addr = await nonFunToken.getAddress();
-
         const msg = ethers.solidityPacked(
             ['address', 'string', 'string', 'string', 'uint256', 'string', 'address', 'uint256'],
-            [adminAccount.address, 'DEPOSIT', 'ERC721', 'TOKENID', 0, 'CONTRACT_ADDRESS', addr, 0]
+            [
+                gameDeveloperAccount.address,
+                'DEPOSIT',
+                'ERC721',
+                'TOKENID',
+                0,
+                'CONTRACT_ADDRESS',
+                nonFunTokenContractAddress,
+                0,
+            ]
         );
-        const signature = await adminAccount.signMessage(msg);
-        console.log('Signed the deposit! Signature: ', signature);
+        const msgHash = ethers.keccak256(msg);
+        const signature = (await adminAccount.provider.send('eth_sign', [adminAccount.address, msgHash])) as string;
+
+        // set approval for all for quest reward contract
+        await nonFunToken.connect(gameDeveloperAccount).setApprovalForAll(questRewardContractAddress, true);
 
         // game developer deposits NFT
-        await questReward.connect(gameDeveloperAccount).depositERC721(addr, 0, signature);
+        await questReward.connect(gameDeveloperAccount).depositERC721(nonFunTokenContractAddress, 0, signature);
 
         const nftOwner = await nonFunToken.ownerOf(0);
-        expect(nftOwner).to.equal('');
+        expect(nftOwner).to.equal(questRewardContractAddress);
 
         // get signature from admin account to withdraw
         // withdraw
