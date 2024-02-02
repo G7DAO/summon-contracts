ef52666481261a50881904299f42db2559cc7da1
ogarciarevett <ogarciarevett@gmail.com>
2024-02-02 10:41:11 -0300

    chore: Testing verify in mumbai

--- constants/deployments/deployments-polygon-mumbai.ts
+++ constants/deployments/deployments-polygon-mumbai.ts
@@ -1,10 +1,25 @@
-import { CONTRACT_UPGRADABLE_FILE_NAME, CONTRACT_UPGRADABLE_NAME, CONTRACT_TYPE } from '@constants/contract';
+import { CONTRACT_TYPE, CONTRACT_FILE_NAME, CONTRACT_NAME } from '@constants/contract';
 import { TENANT } from '@constants/tenant';
 
 import { DeploymentContract } from '../../types/deployment-type';
 import { NETWORK_TYPE, NetworkName } from '../network';
+import { ItemBoundAzerothArgs } from '@constants/constructor-args';
 
 const chain = NetworkName.PolygonMumbai;
 const networkType = NETWORK_TYPE.TESTNET;
 
-export const POLYGON_MUMBAI_CONTRACTS: DeploymentContract[] = [];
+export const POLYGON_MUMBAI_CONTRACTS: DeploymentContract[] = [
+    {
+        contractFileName: CONTRACT_FILE_NAME.ERC1155Soulbound,
+        type: CONTRACT_TYPE.Items,
+        name: CONTRACT_NAME.Items,
+        chain,
+        networkType,
+        tenants: [TENANT.Azeroth],
+        verify: false,
+        upgradable: false,
+        dependencies: [],
+        functionCalls: [],
+        args: ItemBoundAzerothArgs.MAINNET,
+    },
+];
