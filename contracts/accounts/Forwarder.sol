6d3ebbfad3ea9d3fe435c528142edd9244fb3b22
ogarciarevett <ogarciarevett@gmail.com>
2024-10-05 20:53:44 +0200

    Tests: Adding more unit tests to our forwarder and improving checks

--- contracts/accounts/Forwarder.sol
+++ contracts/accounts/Forwarder.sol
@@ -6,7 +6,7 @@ import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.s
 import { Pausable } from "@openzeppelin/contracts/utils/Pausable.sol";
 
 /// @title Forwarder
-/// @notice This contract handles deposits and withdrawals of native tokens, forwarding deposits to a parent address
+/// @notice This contract handles deposits of native tokens, forwarding deposits to a parent address
 contract Forwarder is
     AccessControl,
     Pausable,
@@ -18,12 +18,23 @@ contract Forwarder is
     bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
     bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
     address public parentAddress;
+    mapping(address => uint256) public deposits;
+    uint256 public minDeposit = 0.00001 ether;
 
     /*//////////////////////////////////////////////////////////////
                                  EVENTS
     //////////////////////////////////////////////////////////////*/
-    event ForwarderDeposited(address indexed user, address indexed payer, uint256 value);
+    event ForwarderDeposited(address indexed user, address indexed payer, uint256 value, address indexed parentAddress);
     event ParentAddressUpdated(address newParentAddress);
+    event MinDepositUpdated(uint256 newMinDeposit);
+
+    /*//////////////////////////////////////////////////////////////
+                               MODIFIERS
+   //////////////////////////////////////////////////////////////*/
+    modifier validDeposit() {
+        if(msg.value < minDeposit) revert InvalidAmount();
+        _;
+    }
 
     /*//////////////////////////////////////////////////////////////
                                  ERRORS
@@ -32,6 +43,7 @@ contract Forwarder is
     error InvalidParentAddress();
     error InsufficientBalance();
     error InvalidAmount();
+    error InvalidMinDeposit();
 
     /// @notice Constructs the Forwarder contract
     /// @param _parentAddress The address to which deposits will be forwarded
@@ -49,15 +61,13 @@ contract Forwarder is
                            EXTERNAL FUNCTIONS
     //////////////////////////////////////////////////////////////*/
     /// @notice Allows a user to withdraw their balance
-    function deposit() external payable nonReentrant whenNotPaused {
-        if(msg.value == 0) revert InvalidAmount();
+    function deposit() external payable nonReentrant whenNotPaused validDeposit {
         _handleDeposit(msg.sender, msg.value);
     }
 
     /// @notice Allows a user to withdraw their balance
     /// @param user The address of the user that will receive the deposit
-    function depositTo(address user) external payable nonReentrant whenNotPaused {
-        if(msg.value == 0) revert InvalidAmount();
+    function depositTo(address user) external payable nonReentrant whenNotPaused validDeposit {
         _handleDeposit(user, msg.value);
     }
 
@@ -67,6 +77,14 @@ contract Forwarder is
         if (newParentAddress == address(0)) revert InvalidParentAddress();
         parentAddress = newParentAddress;
         emit ParentAddressUpdated(newParentAddress);
+    }
+
+    /// @notice Updates the minimum deposit amount
+    /// @param newMinDeposit The new minimum deposit amount
+    function updateMinDeposit(uint256 newMinDeposit) external onlyRole(ADMIN_ROLE) {
+        if (newMinDeposit == 0) revert InvalidMinDeposit();
+        minDeposit = newMinDeposit;
+        emit MinDepositUpdated(newMinDeposit);
     }
 
     /// @notice Pauses the contract
@@ -80,14 +98,12 @@ contract Forwarder is
     }
 
     /// @notice Handles incoming native token transfers
-    receive() external payable whenNotPaused {
-        if(msg.value == 0) revert InvalidAmount();
+    receive() external payable whenNotPaused validDeposit {
         _handleDeposit(msg.sender, msg.value);
     }
 
     /// @notice Allows deposits on behalf of a specified user
-    fallback() external payable whenNotPaused {
-        if(msg.value == 0) revert InvalidAmount();
+    fallback() external payable whenNotPaused validDeposit {
         _handleDeposit(msg.sender, msg.value);
     }
 
@@ -100,6 +116,6 @@ contract Forwarder is
     function _handleDeposit(address user, uint256 amount) internal {
         (bool success, ) = parentAddress.call{value: amount}("");
         if (!success) revert ForwardFailed();
-        emit ForwarderDeposited(user, msg.sender, amount);
+        emit ForwarderDeposited(user, msg.sender, amount, parentAddress);
     }
 }
--- test/hardhatTests/forwarder.test.ts
+++ test/hardhatTests/forwarder.test.ts
@@ -21,7 +21,7 @@ export async function setupFixture() {
     return { parentWallet, erc20, admin, user, user2, user3, forwarder, provider, forwarderWithAdmin };
 }
 
-describe.only('Forwarder', function () {
+describe('Forwarder', function () {
     it('Should deploy successfully a forwarder contract', async function () {
         const { forwarder } = await loadFixture(setupFixture);
         expect(await forwarder.getAddress()).to.be.properAddress;
@@ -33,7 +33,9 @@ describe.only('Forwarder', function () {
         const amount = ethers.parseEther('1');
         const initialBalance = await provider.getBalance(await parentWallet.getAddress());
         const tx = await user.sendTransaction({ to: forwarder.target, value: amount });
-        await expect(tx).to.emit(forwarder, 'ForwarderDeposited').withArgs(user.address, user.address, amount);
+        await expect(tx)
+            .to.emit(forwarder, 'ForwarderDeposited')
+            .withArgs(user.address, user.address, amount, parentWallet);
         await tx.wait();
         const finalBalance = await provider.getBalance(await parentWallet.getAddress());
         expect(finalBalance).to.equal(amount + initialBalance);
@@ -46,7 +48,9 @@ describe.only('Forwarder', function () {
         const amount = ethers.parseEther('1');
         const initialBalance = await provider.getBalance(await parentWallet.getAddress());
         const tx = await forwarderWithUser.deposit({ value: amount });
-        await expect(tx).to.emit(forwarder, 'ForwarderDeposited').withArgs(user.address, user.address, amount);
+        await expect(tx)
+            .to.emit(forwarder, 'ForwarderDeposited')
+            .withArgs(user.address, user.address, amount, parentWallet);
         await tx.wait();
         const finalBalance = await provider.getBalance(await parentWallet.getAddress());
         expect(finalBalance).to.equal(amount + initialBalance);
@@ -59,9 +63,101 @@ describe.only('Forwarder', function () {
         const amount = ethers.parseEther('1');
         const initialBalance = await provider.getBalance(await parentWallet.getAddress());
         const tx = await forwarderWithUser1.depositTo(user2, { value: amount });
-        await expect(tx).to.emit(forwarder, 'ForwarderDeposited').withArgs(user2.address, user.address, amount);
+        await expect(tx)
+            .to.emit(forwarder, 'ForwarderDeposited')
+            .withArgs(user2.address, user.address, amount, parentWallet);
         await tx.wait();
         const finalBalance = await provider.getBalance(await parentWallet.getAddress());
         expect(finalBalance).to.equal(amount + initialBalance);
+    });
+
+    it('Should revert when trying to deposit 0 amount', async function () {
+        const { forwarder, user } = await loadFixture(setupFixture);
+        const forwarderWithUser = forwarder.connect(user);
+        await expect(forwarderWithUser.deposit({ value: 0 })).to.be.revertedWithCustomError(forwarder, 'InvalidAmount');
+    });
+
+    it('Should revert when trying to depositTo with 0 amount', async function () {
+        const { forwarder, user, user2 } = await loadFixture(setupFixture);
+        const forwarderWithUser = forwarder.connect(user);
+        await expect(forwarderWithUser.depositTo(user2.address, { value: 0 })).to.be.revertedWithCustomError(
+            forwarder,
+            'InvalidAmount'
+        );
+    });
+
+    it('Should allow admin to update parent address', async function () {
+        const { forwarderWithAdmin, user } = await loadFixture(setupFixture);
+        const newParentAddress = user.address;
+        await expect(forwarderWithAdmin.updateParentAddress(newParentAddress))
+            .to.emit(forwarderWithAdmin, 'ParentAddressUpdated')
+            .withArgs(newParentAddress);
+        expect(await forwarderWithAdmin.parentAddress()).to.equal(newParentAddress);
+    });
+
+    it('Should revert when non-admin tries to update parent address', async function () {
+        const { forwarder, user } = await loadFixture(setupFixture);
+        const forwarderWithUser = forwarder.connect(user);
+        await expect(forwarderWithUser.updateParentAddress(user.address)).to.be.reverted;
+    });
+
+    it('Should revert when trying to update parent address to zero address', async function () {
+        const { forwarderWithAdmin } = await loadFixture(setupFixture);
+        await expect(forwarderWithAdmin.updateParentAddress(ethers.ZeroAddress)).to.be.revertedWithCustomError(
+            forwarderWithAdmin,
+            'InvalidParentAddress'
+        );
+    });
+
+    it('Should allow pauser to pause the contract', async function () {
+        const { forwarderWithAdmin } = await loadFixture(setupFixture);
+        await expect(forwarderWithAdmin.pause()).to.not.be.reverted;
+        expect(await forwarderWithAdmin.paused()).to.be.true;
+    });
+
+    it('Should allow pauser to unpause the contract', async function () {
+        const { forwarderWithAdmin } = await loadFixture(setupFixture);
+        await forwarderWithAdmin.pause();
+        await expect(forwarderWithAdmin.unpause()).to.not.be.reverted;
+        expect(await forwarderWithAdmin.paused()).to.be.false;
+    });
+
+    it('Should revert when non-pauser tries to pause the contract', async function () {
+        const { forwarder, user } = await loadFixture(setupFixture);
+        const forwarderWithUser = forwarder.connect(user);
+        await expect(forwarderWithUser.pause()).to.be.reverted;
+    });
+
+    it('Should revert deposits when contract is paused', async function () {
+        const { forwarderWithAdmin, user } = await loadFixture(setupFixture);
+        await forwarderWithAdmin.pause();
+        const forwarderWithUser = forwarderWithAdmin.connect(user);
+        await expect(forwarderWithUser.deposit({ value: ethers.parseEther('1') })).to.be.revertedWithCustomError(
+            forwarderWithUser,
+            'EnforcedPause'
+        );
+    });
+
+    it('Should handle receive function correctly', async function () {
+        const { forwarder, user, parentWallet, provider } = await loadFixture(setupFixture);
+        const amount = ethers.parseEther('1');
+        const initialBalance = await provider.getBalance(parentWallet.address);
+        await expect(user.sendTransaction({ to: forwarder.target, value: amount }))
+            .to.emit(forwarder, 'ForwarderDeposited')
+            .withArgs(user.address, user.address, amount, parentWallet);
+        const finalBalance = await provider.getBalance(parentWallet.address);
+        expect(finalBalance).to.equal(initialBalance + amount);
+    });
+
+    it('Should handle fallback function correctly', async function () {
+        const { forwarder, user, parentWallet, provider } = await loadFixture(setupFixture);
+        const amount = ethers.parseEther('1');
+        const initialBalance = await provider.getBalance(parentWallet.address);
+        const data = '0x1234'; // Some non-empty calldata
+        await expect(user.sendTransaction({ to: forwarder.target, value: amount, data }))
+            .to.emit(forwarder, 'ForwarderDeposited')
+            .withArgs(user.address, user.address, amount, parentWallet);
+        const finalBalance = await provider.getBalance(parentWallet.address);
+        expect(finalBalance).to.equal(initialBalance + amount);
     });
 });
