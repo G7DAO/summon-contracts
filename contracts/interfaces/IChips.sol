a39b3e333a43809944fe495b54d61897a44aaefc
mrk-hub <mrkennedy@game7.io>
2025-04-10 12:41:56 -0400

    Chore: Initial Build

---
+++ contracts/interfaces/IChips.sol
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+interface IChips {
+    function deposit(uint256 amount) external;
+
+    function withdraw(uint256 amount) external;
+
+    function retrieveBuyIn(address from, uint256 amount) external;
+
+    function distributeChips(
+        address[] memory users,
+        uint256[] memory amounts
+    ) external;
+
+    function balanceOf(address account) external view returns (uint256);
+
+    function totalSupply() external view returns (uint256);
+}
