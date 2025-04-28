23282c9b802ecbdfbc19479835f2c642950afdd6
Kellan Wampler <kellan@moonstream.to>
2025-04-28 08:35:48 -0400

    Chore: Refactor chip/currency exchange calcuation

--- contracts/upgradeables/chips/Chips.sol
+++ contracts/upgradeables/chips/Chips.sol
@@ -173,8 +173,7 @@ contract Chips is
     {
         uint256 amount = _verifyContractChainIdAndDecode(data);
         token.safeTransferFrom(msg.sender, address(this), amount);
-        uint256 amountInChips = (amount * numeratorExchangeRate) /
-            denominatorExchangeRate;
+        uint256 amountInChips = _parseCurrencyToChips(amount);
         _update(address(0), msg.sender, amountInChips);
         emit Deposit(msg.sender, amountInChips);
     }
@@ -193,8 +192,7 @@ contract Chips is
     {
         uint256 amountInChips = _verifyContractChainIdAndDecode(data);
         _update(msg.sender, address(0), amountInChips);
-        uint256 amountInTokens = (amountInChips * denominatorExchangeRate) /
-            numeratorExchangeRate;
+        uint256 amountInTokens = _parseChipsToCurrency(amountInChips);
         token.safeTransfer(msg.sender, amountInTokens);
         emit Withdraw(msg.sender, amountInTokens);
     }
@@ -212,8 +210,7 @@ contract Chips is
         }
         for (uint256 i = 0; i < users.length; i++) {
             token.safeTransferFrom(admin, address(this), amounts[i]);
-            uint256 amountInChips = (amounts[i] * numeratorExchangeRate) /
-                denominatorExchangeRate;
+            uint256 amountInChips = _parseCurrencyToChips(amounts[i]);
             _update(address(0), users[i], amountInChips);
         }
         emit AdminDeposit(admin, users, amounts);
@@ -226,10 +223,11 @@ contract Chips is
     ) external onlyRole(MANAGER_ROLE) whenPaused nonReentrant {
         for (uint256 i = 0; i < users.length; i++) {
             uint256 balance = _balances[users[i]];
-            uint256 amountInTokens = (balance * denominatorExchangeRate) /
-                numeratorExchangeRate;
-            _update(users[i], address(0), balance);
-            token.safeTransfer(users[i], amountInTokens);
+            if (balance > 0) {
+                uint256 amountInTokens = _parseChipsToCurrency(balance);
+                _update(users[i], address(0), balance);
+                token.safeTransfer(users[i], amountInTokens);
+            }
         }
         emit WithdrawAllAdmin(_msgSender(), users);
     }
@@ -270,6 +268,14 @@ contract Chips is
     // @dev Unpauses the contract
     function unpause() external onlyRole(MANAGER_ROLE) {
         _unpause();
+    }
+
+    function _parseCurrencyToChips(uint256 currencyBalance) internal view returns (uint256) {
+        return (currencyBalance * numeratorExchangeRate) / denominatorExchangeRate;
+    }
+
+    function _parseChipsToCurrency(uint256 chipsBalance) internal view returns (uint256) {
+        return (chipsBalance * denominatorExchangeRate) / denominatorExchangeRate;
     }
 
     // @dev Returns the balance of the user
